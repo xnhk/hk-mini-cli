14eac7f280d663fd1edfd99ea6da4f238a5419ea
huangke <huangke@cyberway.net.cn>
2020-08-24 13:35:05 +0800

    添加app公共方法

--- src/app.js
+++ src/app.js
@@ -9,5 +9,43 @@ App({
   $bus: new eventBus(),
   globalData: {
     userInfo: null
-  }
+  },
+  getPage(index) {
+    let num = index || 1
+    var curPages =  getCurrentPages();
+    if(curPages.length < 2) return false
+    return curPages[curPages.length - (num + 1)]
+  },
+  commonModal(content, fn) {
+    wx.showModal({
+      title: '提示',
+      content,
+      success (res) {
+        if (res.confirm) {
+          fn && fn()
+        } 
+      }
+    }) 
+  },
+  commonToast(title, icon) {
+    wx.showToast({
+      title,
+      duration: 2500,
+      icon: icon || 'none'
+    })
+  },
+  // 节流
+  throttle(fn, delay) {
+    let timer = null 
+    return function() {
+      let context = this,
+      args = arguments
+      if(!timer) {
+        timer = setTimeout(function(){
+          fn.apply(context, args);
+          timer = null;
+        }, delay);
+      }
+    }
+  },
 })
